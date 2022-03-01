4cb227b7bb8bdfa7098b938182ca0ff2804cd32c
jon halbert <johnny.halbert@gmail.com>
2022-02-28 20:09:07 -0600

    Added event listeners to takeoff button

--- scripts.js
+++ scripts.js
@@ -1,2 +1,23 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListner('load', function(){
+
+
+    let takeoff = document.getElementById('takeoff');
+    let status =document.getElementById('flightStatus');
+    let shuttleHeight =document.getElementById('shuttleBackground');
+    let background = document.getElementById('spaceShuttleHeight');
+
+    takeoff.addEventListener("click", function(){
+        result = window.confirm('Confirm that the shuttle is ready for takeoff.');
+        if (result) {
+            status.innerHTML = 'Shuttle in flight.'
+            background.style.backgroundColor = 'blue';
+            shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML) + 10000;
+        }
+    })
+
+    let landing = document.getElementById('landing');
+
+    let missionAbort = document.getElementById('missionAbort');
+})
